22cfb3370a0c40e0c5eb11d0e1318b815f3f5cc7
MJ <dtwogud@nate.com>
2025-03-05 16:16:16 +0900

    feat

--- src/main/java/com/shop/cafe/controller/MemberController.java
+++ src/main/java/com/shop/cafe/controller/MemberController.java
@@ -1,14 +1,33 @@
 package com.shop.cafe.controller;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.shop.cafe.dto.Member;
+import com.shop.cafe.service.MemberService;
 
 @RestController
 public class MemberController {
 	
-	@GetMapping("insertMember")
-	public String insertMember() {
-		return "ok";
+	@Autowired
+	MemberService memberService;
+	
+	@PostMapping("insertMember")
+	public Map<String, String> insertMember(@RequestBody Member m) {
+		Map<String, String> responseData = new HashMap();
+		try {
+			memberService.insertMember(m);
+			responseData.put("msg","ok");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			responseData.put("msg",e.getMessage());
+		}
+		return responseData;
 	}
-
-}
+}  
